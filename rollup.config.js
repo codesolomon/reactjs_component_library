724c0c9830061595682272294745a26c847235a9
codesolomon <solomonkalejaiye@gmail.com>
2022-09-27 16:25:26 +0100

    Reuseable library
    build: implement storybook, jest and react testing library and babel for debugging an d testing as well as rollup for js bundler

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
 const packageJson = require("./package.json");
 
 export default [
@@ -24,11 +28,15 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      peerDepsExternal(),
+      terser(),
     ],
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
